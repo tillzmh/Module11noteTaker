413134faead9e8cdd3c8672e0b64322166eaede8
tillzmh <tillzmh@users.noreply.github.com>
2022-09-06 21:12:22 -0400

    fixed listening port

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.post('/api/notes', function(req, res){
         res.send()
     })
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('you listen on this port?')
 })
